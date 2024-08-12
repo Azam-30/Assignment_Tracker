2388aa0cd203c44476a0165b97cc881baff889a5
shaik almas azam <shaikalmasazam@shaiks-Air.lan>
2024-08-12 21:32:05 +0530

    Fix static files configuration for fifth time

--- hello/settings.py
+++ hello/settings.py
@@ -32,7 +32,6 @@ ALLOWED_HOSTS = ['.vercel.app','.now.sh','127.0.0.1','localhost','assignment-tra
 # Application definition
 
 INSTALLED_APPS = [
-    "whitenoise.runserver_nostatic",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -44,7 +43,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -130,7 +128,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 
 STATIC_URL = '/static/'
