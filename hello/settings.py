59e81bbd09149dd1636d8cbc5f28bdb71655d3f3
shaik almas azam <shaikalmasazam@shaiks-Air.lan>
2024-08-12 20:36:54 +0530

    Fix static files configuration for fourth time

--- hello/settings.py
+++ hello/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-p6uy$50f5r$co6b@^q3j6oqa%8%)*eaj2e47+^s0h4tuxq3xvi"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.vercel.app','.now.sh','127.0.0.1','localhost','assignment-tracker-i5d6cot0v-shaik-almas-azams-projects.vercel.app']
 
