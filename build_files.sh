92f67a99e8efbf1871ae90ffc93f7166d5cbf936
shaik almas azam <shaikalmasazam@shaiks-Air.lan>
2024-08-12 19:07:53 +0530

    Fix static files configuration for second time

--- build_files.sh
+++ build_files.sh
@@ -5,3 +5,6 @@ pip install -r requirements.txt
 
 echo "Collecting static files..."
 python3.9 manage.py collectstatic --noinput
+
+echo "Applying database migrations..."
+python3.9 manage.py migrate
