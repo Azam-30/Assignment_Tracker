ece8b08e6590462719088da54f8a6b675c55871d
shaik almas azam <shaikalmasazam@shaiks-Air.lan>
2024-08-13 19:32:33 +0530

    Fixing static files through whitenoise

--- hello/settings.py
+++ hello/settings.py
@@ -39,12 +39,10 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     'home',
-    'whitenoise.runserver_nostatic',
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
